519db371e7b197148ffeefd0fcdf7ed65574d8e8
bveedu <byjupv@gmail.com>
2010-02-26 17:30:32 +0530

    Change in SelfActor

--- SelfActor.scala
+++ SelfActor.scala
@@ -1,5 +1,6 @@
 import scala.actors.Actor._
 // Accessing the Actor related to current thread
 // Sending and recieving message to self (Current thread) 
- self ! "Hello world"
+
+ self ! "Hello world to self"
  self.receive { case x => println("Recieved self :"+x) }
